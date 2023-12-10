4ede463f20e5b2d27f3c315a462b498e49789375
Nikita Panov <panov.nv@phystech.edu>
2023-12-11 02:43:55 +0300

    Fix

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PID_FILE="monitoringd.pid"
-
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 case $1 in
 
     STATUS)
-        if test -f "$PID_FILE"; then
+        if test -f "$PARENT_PATH/$PID_FILE"; then
             echo "Daemon is working"
         else
             echo "Daemon is not working"
@@ -14,24 +14,24 @@ case $1 in
 
     START)
 
-        if test -f "$PID_FILE"; then
+        if test -f "$PARENT_PATH/$PID_FILE"; then
             echo "Daemon already working"
         else
 
-            ./monitoring.sh &
+            $PARENT_PATH/monitoring.sh &
 
             DAEMON_PID=$!
 
-            echo $DAEMON_PID > $PID_FILE
+            echo $DAEMON_PID > $PARENT_PATH/$PID_FILE
         fi
 
     ;;
 
     STOP)
-        if test -f "$PID_FILE"; then
-            DAEMON_PID=$(cat $PID_FILE)
+        if test -f "$PARENT_PATH/$PID_FILE"; then
+            DAEMON_PID=$(cat $PARENT_PATH/$PID_FILE)
             kill -15 $DAEMON_PID
-            rm $PID_FILE
+            rm $PARENT_PATH/$PID_FILE
         else
             echo "Can't stop daemon, because it is not working"
         fi
